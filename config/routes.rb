e3c468590c24c88e3d701752b9b06eb4f88a8a13
Sanjeev Poudel <sanjeevpoudel111@gmail.com>
2021-09-06 13:29:28 +0545

    initial commit

---
+++ config/routes.rb
@@ -0,0 +1,18 @@
+# For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+Rails.application.routes.draw do
+  # GET /about routes to index action of about controller
+  
+  # get '/about', to: 'about#index' here, '/' is optional
+  
+  # or if link_to helper is used in views
+  # get "about-us", to:'about#index', as: :about
+  #or
+  get 'about',to:'about#index'
+  
+  # Root route
+  # get '/', to:'main#index' or
+  # root to:'main#index' or
+  root 'main#index'
+  
+end
