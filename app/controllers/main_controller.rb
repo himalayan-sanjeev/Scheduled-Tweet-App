8b41516a9971ede0fa594fd1e1f180814c2c0b49
Sanjeev Poudel <sanjeevpoudel111@gmail.com>
2021-09-06 13:49:31 +0545

    flash messages added

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,6 +1,9 @@
 class MainController < ApplicationController
   def index
-  
+    flash[:notice]="Logged in Successfully"
+    flash[:alert]="Invalid Entries"
+    #flash.now[:alert]="Invalid Entries"
+
   end
-  
+
 end
